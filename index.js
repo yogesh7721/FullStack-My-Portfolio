97950dacd014958ced5021ebfe1baf40a0b1e27c
yogesh shelke <shelkeyogesh874@gmail.com>
2024-09-04 20:31:13 +0530

    server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config({ path: "./.env" })   // env File Path
 
 const app = express()
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true}))
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 app.use(cookieParser())
 app.use(express.json())
 
